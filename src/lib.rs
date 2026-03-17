723bcfc0066629ead257f2440734a8d933eff87c
Andrew Solokhin <andrewsoloh@gmail.com>
2026-03-17 14:58:20 +0300

    Added elements processing

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use crate::RpnElements::{Operator, Value};
 use crate::RpnError::ParseError;
 
 #[derive(PartialEq, Debug)]
@@ -27,6 +28,28 @@ fn processing_data(input: &str) -> Result<Vec<String>, RpnError> {
     let mut stack: Vec<String> = Vec::new();
     for element in input.split_whitespace() {
         stack.push(element.to_string());
+    }
+
+    if stack.is_empty() {
+        return Err(ParseError(input.to_string()));
+    }
+    Ok(stack)
+}
+
+fn processing_elements(input: &str) -> Result<Vec<RpnElements>, RpnError> {
+    let mut stack: Vec<RpnElements> = Vec::new();
+    for element in input.split_whitespace() {
+        if let Ok(i) = element.parse::<f64>() {
+            stack.push(Value(i));
+        } else {
+            match element {
+                "+" => stack.push(Operator(RpnOperator::Add)),
+                "-" => stack.push(Operator(RpnOperator::Sub)),
+                "*" => stack.push(Operator(RpnOperator::Mul)),
+                "/" => stack.push(Operator(RpnOperator::Div)),
+                _ => return Err(ParseError(input.to_string())),
+            }
+        }
     }
 
     if stack.is_empty() {
